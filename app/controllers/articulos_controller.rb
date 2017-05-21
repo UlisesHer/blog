824c2c622629e99367b0176c32a7ab236b61ccdc
Alexander Hernandez <smis342113@gmail.com>
2017-05-21 11:11:39 -0600

    agrega mvc de articulos

---
+++ app/controllers/articulos_controller.rb
@@ -0,0 +1,52 @@
+class ArticulosController < ApplicationController
+
+  def index
+    @articulos = Articulo.order("created_at desc")
+  end
+
+  def show
+     @articulo = Articulo.find(params[:id])
+  end
+
+  def new
+    @articulo = Articulo.new
+  end
+  
+  def create
+    @articulo = Articulo.new(articulo_params)
+    
+    if @articulo.save
+      redirect_to @articulo
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @articulo = Articulo.find(params[:id])
+  end
+
+  def update
+    @articulo = Articulo.find(params[:id])
+   
+    if @articulo.update(articulo_params)
+      redirect_to @articulo
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @articulo = Articulo.find(params[:id])
+    @articulo.destroy
+   
+    redirect_to articulos_path
+  end
+
+  private
+  
+  def articulo_params
+    params.require(:articulo).permit(:titular, :contenido)
+  end
+
+end
